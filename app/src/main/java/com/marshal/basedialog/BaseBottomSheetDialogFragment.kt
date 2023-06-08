e88fbf7490a27809ec958eacb3fdc0254f85158a
marshal <1147083532@qq.com>
2023-06-08 18:19:10 +0800

    1.dialog添加base基类

---
+++ app/src/main/java/com/marshal/basedialog/BaseBottomSheetDialogFragment.kt
@@ -0,0 +1,43 @@
+package com.marshal.basedialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+abstract class BaseBottomSheetDialogFragment<T: ViewBinding>: BottomSheetDialogFragment() {
+
+    var rootView:View? = null
+    var binding:T? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        rootView = inflater.inflate(getResLayoutId(), container)
+
+        return getResLayoutBinding()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        initView()
+
+        subscribeBack()
+    }
+
+
+    abstract fun getResLayoutId():Int
+
+    abstract fun getResLayoutBinding(): View?
+
+    abstract fun initView()
+
+    open fun subscribeBack(){}
+
+}
