371cd12d46f78b80e08ea5606edd017dec8f02b8
marshal <1147083532@qq.com>
2023-10-13 16:09:14 +0800

    1.项目中添加calendarview模块 2.开发显示日历和添加日程界面

---
+++ app/src/main/java/com/marshal/calendar/CalendarActivity.java
@@ -0,0 +1,146 @@
+package com.marshal.calendar;
+
+import android.annotation.SuppressLint;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.github.gzuliyujiang.wheelpicker.contract.OnDatePickedListener;
+import com.google.android.material.tabs.TabLayout;
+import com.haibin.calendarview.Calendar;
+import com.haibin.calendarview.CalendarView;
+import com.marshal.AppRouterPath;
+import com.marshal.base_common.baseview.BaseViewActivity;
+import com.marshal.databinding.ActivityAddCalendarBinding;
+
+@Route(path = AppRouterPath.APP_ADD_EVENT_CALENDAR)
+public class CalendarActivity extends BaseViewActivity<ActivityAddCalendarBinding>{
+
+
+
+    private CalendarView.OnYearChangeListener onYearListener = year -> {
+        getBinding().tvMonthDay.setText(String.valueOf(year));
+    };
+
+    private CalendarView.OnCalendarSelectListener onCalendarSelectListener = new CalendarView.OnCalendarSelectListener() {
+        @Override
+        public void onCalendarOutOfRange(Calendar calendar) {
+
+        }
+
+        @Override
+        public void onCalendarSelect(Calendar calendar, boolean isClick) {
+            getBinding().tvShowYear.setText(calendar.getYear()+"年");
+            getBinding().tvMonthDay.setText(calendar.getMonth() + "月" + calendar.getDay() + "日");
+        }
+    };
+
+
+    @Override
+    public boolean hasToolbar() {
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public View getResLayoutBinding() {
+        setBinding(ActivityAddCalendarBinding.inflate(getLayoutInflater()));
+        return getBinding().getRoot();
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void initView() {
+
+        if(getHasIncludeToolbar()) {
+            setTitle("显示日历");
+        }
+
+        getBinding().tvShowYear.setText(getBinding().calendarView.getCurYear()+"年");
+        getBinding().tvMonthDay.setText(getBinding().calendarView.getCurMonth() + "月" + getBinding().calendarView.getCurDay() + "日");
+
+        getBinding().calendarView.setOnYearChangeListener(onYearListener);
+        getBinding().calendarView.setOnCalendarSelectListener(onCalendarSelectListener);
+
+        getBinding().ivShowPopupWindow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DateSelectUtils.INSTANCE.showDateSelectDialog(CalendarActivity.this, new OnDatePickedListener() {
+                    @Override
+                    public void onDatePicked(int year, int month, int day) {
+
+                        getBinding().calendarView.scrollToCalendar(year,month,day,false,true);
+                    }
+                });
+            }
+        });
+
+        getBinding().tabLayout.addTab(getBinding().tabLayout.newTab().setId(1).setText("月"));
+        getBinding().tabLayout.addTab(getBinding().tabLayout.newTab().setId(2).setText("周"));
+        getBinding().tabLayout.addTab(getBinding().tabLayout.newTab().setId(3).setText("日"));
+        getBinding().tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch (tab.getId()) {
+                    case 1:
+                        switchScheduleView(true);
+                        break;
+                    case 2:
+                    case 3:
+                        switchScheduleView(false);
+                        break;
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+//        getBinding().tabMonth.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                switchScheduleView(true);
+//            }
+//        });
+//
+//        getBinding().tabWeek.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                switchScheduleView(false);
+//            }
+//        });
+//
+//        getBinding().tabDay.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                switchScheduleView(false);
+//            }
+//        });
+
+    }
+
+
+    private void switchScheduleView(boolean showMonth){
+        if(showMonth) {
+            getBinding().nestedScrollView.setVisibility(View.VISIBLE);
+            getBinding().flnShowSchedule.setVisibility(View.GONE);
+            getBinding().calendarLayout.setModeBothMonthWeekView();
+            getBinding().calendarLayout.expand(150);
+        }else {
+            getBinding().nestedScrollView.setVisibility(View.GONE);
+            getBinding().flnShowSchedule.setVisibility(View.VISIBLE);
+            getBinding().calendarLayout.setModeOnlyMonthView();
+            getBinding().calendarLayout.shrink(150);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/marshal/calendar/DateSelectUtils.kt
@@ -0,0 +1,39 @@
+package com.marshal.calendar
+
+import android.app.Activity
+import com.github.gzuliyujiang.wheelpicker.DatePicker
+import com.github.gzuliyujiang.wheelpicker.annotation.DateMode
+import com.github.gzuliyujiang.wheelpicker.contract.OnDatePickedListener
+import com.github.gzuliyujiang.wheelpicker.entity.DateEntity
+import com.github.gzuliyujiang.wheelpicker.impl.UnitDateFormatter
+import com.github.gzuliyujiang.wheelpicker.widget.DateWheelLayout
+import com.marshal.R
+import java.util.Date
+
+object DateSelectUtils {
+
+    fun showDateSelectDialog(context:Activity,onDatePickedListener: OnDatePickedListener){
+        val datePicker = DatePicker(context)
+        val dateWheelLayout: DateWheelLayout = datePicker.wheelLayout
+        dateWheelLayout.setDateMode(DateMode.YEAR_MONTH_DAY)
+        dateWheelLayout.setDateFormatter(UnitDateFormatter())
+        val startDate = Date(946656000000)//2000.1.1
+        val startEntity = DateEntity.target(startDate)
+        val endDate = Date(4102250639000)//2099.12.30
+        val endEntity = DateEntity.target(endDate)
+        dateWheelLayout.setRange(startEntity, endEntity)
+        dateWheelLayout.setDefaultValue(DateEntity.today())
+        dateWheelLayout.setCurtainEnabled(true)
+        dateWheelLayout.setCurtainColor(context.resources.getColor(R.color.white))
+        dateWheelLayout.setIndicatorEnabled(true)
+        dateWheelLayout.setIndicatorColor(context.resources.getColor(R.color.white))
+        dateWheelLayout.setIndicatorSize(context.resources.displayMetrics.density * 2)
+        dateWheelLayout.setTextColor(context.resources.getColor(R.color.gray_line))
+        dateWheelLayout.setTextSize(14 * context.resources.displayMetrics.scaledDensity)
+        dateWheelLayout.setSelectedTextColor(context.resources.getColor(R.color.text_color_black))
+        dateWheelLayout.setResetWhenLinkage(false)
+        datePicker.setOnDatePickedListener(onDatePickedListener)
+        datePicker.show()
+    }
+
+}
